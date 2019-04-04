b8b1d628f1654322c361bbca341170aa108dc462
성수 <oplm123456@naver.com>
2019-04-04 20:21:15 +0900

    페이지 전환

--- src/application/DOSApplicationController.java
+++ src/application/DOSApplicationController.java
@@ -1,35 +1,69 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.stage.Stage;
 
 /**
- * @author 전부
- * MainScreen.fxml의 컨트롤러파일 
+ * @author �쟾遺� MainScreen.fxml�쓽 而⑦듃濡ㅻ윭�뙆�씪
  */
-public class DOSApplicationController implements Initializable{
-	@FXML
-	private ImageView backgroundImageView;
+public class DOSApplicationController implements Initializable {
+   @FXML
+   private ImageView backgroundImageView;
+   @FXML
+   private Button btnlogin;
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		Image backgroundImage = (new ImageParser("main.gif").getImage());
-		backgroundImageView.setImage(backgroundImage);
-		Music introMusic = new Music("Game On.mp3", true);
-		try {
-			Thread.sleep(4500);
-			introMusic.start();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
+   @Override
+   public void initialize(URL url, ResourceBundle rb) {
+      Image backgroundImage = (new ImageParser("main.gif").getImage());
+      backgroundImageView.setImage(backgroundImage);
+      Music introMusic = new Music("Game On.mp3", true);
+      try {
+         Thread.sleep(4500);
+         introMusic.start();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   /**
+    * @author 재원,성수 페어(페이지 전환)
+    */
+   public void movePage() {
+
+      // 새 스테이지 추가
+
+      Stage newStage = new Stage();
+
+      Stage stage = (Stage) btnlogin.getScene().getWindow();
+
+      try {
+
+         Parent second = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+
+         // 씬에 레이아웃 추가
+
+         Scene sc = new Scene(second);
+
+         stage.setScene(sc);
+   
+         stage.show();
+
+      } catch (IOException e) {
+
+         e.printStackTrace();
+
+      }
+
+   }
+
 }
