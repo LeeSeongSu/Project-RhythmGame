ea0e2b9b0af19ddb5b0a95170bea331ab0748d24
성수 <oplm123456@naver.com>
2019-04-06 23:54:59 +0900

    메인화면 변경 및 키보드이벤트

--- src/application/DOSApplicationController.java
+++ src/application/DOSApplicationController.java
@@ -7,66 +7,88 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
  * @author 태일 MainScreen.fxml DOSApplication을 컨트롤하는 컨트롤러
  */
-public class DOSApplicationController implements Initializable {
-   @FXML
-   private ImageView backgroundImageView;
-   @FXML
-   private Button loginBtn;
+public class DOSApplicationController extends Thread implements Initializable {
+	@FXML
+	private ImageView backgroundImageView;
+	@FXML
+	private ImageView MainlogoView;// 메인로고
 
-   @Override
-   public void initialize(URL url, ResourceBundle rb) {
-      Image backgroundImage = (new ImageParser("main.gif").getImage());
-      backgroundImageView.setImage(backgroundImage);
-      Music introMusic = new Music("Game On.mp3", true);
-      try {
-         Thread.sleep(4500);
-         introMusic.start();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-      
-      loginBtn.setOnAction(e -> movePage());
-   }
-   
-   
-   /**
-    * @author 재원,성수 페어(페이지 전환)
-    */
-   public void movePage() {
+	@FXML
+	private Button loginBtn;// 로그인 버튼
+	@FXML
+	private Button press;// 아무키나 누르시오
+	@FXML
+	private Group loginscreen;// 로그인 화면
 
-      // 새 스테이지 추가
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Image backgroundImage = (new ImageParser("Main_bg.gif").getImage());
+		backgroundImageView.setImage(backgroundImage);
 
-      Stage stage = (Stage) loginBtn.getScene().getWindow();
+		Music introMusic = new Music("Game On.mp3", true);
+		try {
+			Thread.sleep(4500);
+			introMusic.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-      try {
+		loginBtn.setOnAction(e -> movePage());
+		loginscreen.setVisible(false);// 로그인창 숨김
+	}
 
-         AnchorPane second = FXMLLoader.load(getClass().getResource("SelectScreen.fxml"));
-         
-         Menubar menubar = new Menubar(second);
-         // 씬에 레이아웃 추가
-         Scene sc = new Scene(second);
-         
-         stage.setScene(sc);
-   
-         stage.show();
+	/**
+	 * @author 성수(메인화면에서 아무키나 눌렀을 때)
+	 */
+	public void press() {
 
-      } catch (IOException e) {
+		press.setVisible(false);// 아무키나 누르시오 창 숨김
+		MainlogoView.setLayoutY(-350);// 메인로고 위치
+		loginscreen.setVisible(true);// 로그인창 보임
 
-         e.printStackTrace();
+	}
 
-      }
+	/**
+	 * @author 재원,성수 페어(페이지 전환)
+	 */
+	public void movePage() {
 
-   }
+		// 새 스테이지 추가
+
+		Stage stage = (Stage) loginBtn.getScene().getWindow();
+
+		try {
+
+			AnchorPane second = FXMLLoader.load(getClass().getResource("SelectScreen.fxml"));
+
+			Menubar menubar = new Menubar(second);
+			// 씬에 레이아웃 추가
+			Scene sc = new Scene(second);
+
+			stage.setScene(sc);
+
+			stage.show();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
 
 }
