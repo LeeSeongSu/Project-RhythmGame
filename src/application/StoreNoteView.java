b98116dc76060a00a82c5f01296c493dee2dca4b
성수 <oplm123456@naver.com>
2019-06-10 19:14:19 +0900

    제발 충돌 안나게 해주세요

--- src/application/StoreNoteView.java
+++ src/application/StoreNoteView.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -7,7 +10,8 @@ import javafx.scene.layout.AnchorPane;
 public class StoreNoteView {
 	private AnchorPane pane;
 	private ImageView Background;
-
+	private ArrayList<Button> buttons;
+	private Button btn;
 	public StoreNoteView(AnchorPane pane) {
 		// TODO Auto-generated constructor stub
 		this.pane = pane;
@@ -15,6 +19,21 @@ public class StoreNoteView {
 		Image backGroundImage = (new ImageParser("Store_Note.png").getImage());
 		Background = new ImageView(backGroundImage);
 		pane.getChildren().add(Background);// 로비 배경
+		
+		buttons = new ArrayList<Button>();
+		for (int a = 0; a < 3; a++) {
+			btn = new Button();
+			btn.setPrefSize(480, 818);
+
+			btn.setLayoutX(433 + a * 480);
+			btn.setLayoutY(208);
+			btn.setOpacity(1);
+
+			buttons.add(btn);
+
+			pane.getChildren().add(btn);
+
+		}
 	}
 
 }
