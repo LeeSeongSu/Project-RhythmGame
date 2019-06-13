bac40825221cf0c3bce4849878e3b337a3ed8e3e
Kim DongJin <41800328+kxv1379@users.noreply.github.com>
2019-06-14 08:23:51 +0900

    음성게이지 수정

--- src/application/VolumeBoard.java
+++ src/application/VolumeBoard.java
@@ -12,6 +12,23 @@ public class VolumeBoard {
 	static boolean playing;
 
 	public VolumeBoard(Pane pane) {
+
+		
+		playing=true;
+		Image volumeBoardImage = new ImageParser("Volume.png").getImage();
+		ImageView volumeBoard = new ImageView(volumeBoardImage);
+		volumeBoard.setLayoutX(1500);
+		volumeBoard.setLayoutY(100);
+		
+
+		Rectangle line = new Rectangle(0, 613);//613
+		line.setStroke(Paint.valueOf("#FFFFFF"));
+		line.setStrokeWidth(10.5);
+		line.setLayoutX(1576);
+		line.setLayoutY(162);
+		line.setStrokeType(StrokeType.OUTSIDE);
+		pane.getChildren().add(line);
+		
 		Rectangle block = new Rectangle(0, 613);
 		block.setStroke(Paint.valueOf("#000000"));
 		block.setStrokeWidth(10.5);
@@ -20,23 +37,8 @@ public class VolumeBoard {
 		block.setStrokeType(StrokeType.OUTSIDE);
 		
 		pane.getChildren().add(block);
-		playing=true;
-		Image volumeBoardImage = new ImageParser("Volume.png").getImage();
-		ImageView volumeBoard = new ImageView(volumeBoardImage);
-		volumeBoard.setLayoutX(1500);
-		volumeBoard.setLayoutY(100);
+		
 		pane.getChildren().add(volumeBoard);
-
-//		Rectangle line = new Rectangle(0, 613);//613
-//		line.setStroke(Paint.valueOf("#FFFFFF"));
-//		line.setStrokeWidth(10.5);
-//		line.setLayoutX(1576);
-//		line.setLayoutY(162);
-//		line.setStrokeLineCap(StrokeLineCap.ROUND);
-//		line.setStrokeType(StrokeType.OUTSIDE);
-//		
-//		pane.getChildren().add(line);
-
 		Thread thread = new Thread() {
 			@Override
 			public void run() {
